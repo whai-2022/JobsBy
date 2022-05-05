f8b7a2569c2803bcf960a140f217b27f58606e28
ben-marrett <benteiko@gmail.com>
2022-05-06 10:03:09 +1200

    begin form

--- client/components/PostJob.jsx
+++ client/components/PostJob.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 
+import { VStack, Heading, Text} from '@chakra-ui/react'
+
 function PostJob(){
 
   return (
-    <div>
-      <p>Add form here for new job posting</p>
-    </div>
+    <>
+      <VStack
+        w='full'
+        h='full'
+        p={10}
+        spacing={10}
+        align-items='flex-start'>
+        <Heading size='xl' textAlign='center'> Post a job </Heading>
+        <Text>Please enter the details for your job:</Text>
+      </VStack>
+    </>
   )
 }
 
