42d97642d8db3d4ab93271e9b1e5a05942c4ebef
ben-marrett <benteiko@gmail.com>
2022-05-06 13:00:18 +1200

    add basic box layout for jobs list: no logic included yet, purely layout

--- client/components/AllJobs.jsx
+++ client/components/AllJobs.jsx
@@ -1,5 +1,7 @@
 import React from "react"
 
+import Job from './Job'
+
 import { Select, Heading, VStack} from "@chakra-ui/react"
 
 function AllJobs() {
@@ -7,18 +9,18 @@ function AllJobs() {
   return (
     <>
       <Heading m={8}>Find a job in your region.</Heading>
-      <Select placeholder='Select your location' size='sm'>
+      <Select placeholder='Select your location' size='sm' p={8}>
         <option value='Auckland'>Auckland</option>
         <option value='Wellington'>Wellington</option>
         <option value='Dargaville'>...Dargaville</option>
       </Select>
 
       <VStack>
-        
+      <Job/>
+      <Job />
+      <Job/>
       </VStack>
-
-
-
+      
     </>
   )
 }
