08878ca711ca6e0667e26f7e1eb1498f9a166a13
ben-marrett <benteiko@gmail.com>
2022-05-07 00:35:46 +1200

    remove nav from main container

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -35,7 +35,6 @@ function App() {
         {/* {authorized ? <Home /> : <Splash />} */}
       </section>
       <footer className="footer"></footer>
-      <Nav />
       {/* <LoggedIn /> */}
 
         <Routes>
