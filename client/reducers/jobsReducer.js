c5a98596fc0b14fb62223472fb95691e0eebb789
ben-marrett <benteiko@gmail.com>
2022-05-07 12:30:27 +1200

    add reducer case for get job details

--- client/reducers/jobsReducer.js
+++ client/reducers/jobsReducer.js
@@ -1,22 +1,46 @@
-import { SET_JOBS } from '../actions'
+import { SET_JOBS, GET_JOB_DETAILS } from '../actions'
 
-const initialJobState = [
-  {
-    id: 1,
-    title: 'dishes',
-    description: 'piles and piles of dishes',
-    requirements: 'none',
-    locationRegion: 'Kaipara',
-    locationSuburb: 'Paparoa',
+const initialJobState = {
+  jobs: [
+    {
+      id: 1,
+      title: 'dishes',
+      description: 'piles and piles of dishes',
+      requirements: 'none',
+      locationRegion: 'Kaipara',
+      locationSuburb: 'Paparoa',
+      typeId: 1,
+      pay: '0.001 BTC',
+    },
+    {
+      id: 2,
+      title: 'Clean out my car',
+      description: 'General cleaning and vacuuming in my rusty old Outlander',
+      requirements: 'Elbow grease',
+      locationRegion: 'Auckland',
+      locationSuburb: 'Mt. Eden',
+      typeId: 1,
+      pay: '$40',
+    },
+  ],
+  job: {
+    id: 2,
+    title: 'Clean out my car',
+    description: 'General cleaning and vacuuming in my rusty old Outlander',
+    requirements: 'Elbow grease',
+    locationRegion: 'Auckland',
+    locationSuburb: 'Mt. Eden',
     typeId: 1,
-    pay: '0.001 BTC',
+    pay: '$40',
   },
-]
+}
 
 const jobsReducer = (state = initialJobState, action) => {
   switch (action.type) {
     case SET_JOBS:
       return action.jobs
+    case GET_JOB_DETAILS:
+      return action.job
     default:
       return state
   }
