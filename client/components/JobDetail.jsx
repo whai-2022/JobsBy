eb83d903955452b6427f955cdfbad30c8e1eaba0
ben-marrett <benteiko@gmail.com>
2022-05-07 14:57:36 +1200

    begin work on individual job component: gathering params, fetching jobById. tbc

--- client/components/JobDetail.jsx
+++ client/components/JobDetail.jsx
@@ -1,13 +1,22 @@
-import React from 'react'
+import React, { useEffect, useState} from 'react'
+import { useSelector, useDispatch } from 'react-redux'
 
 import { useParams } from 'react-router-dom'
 import { fetchJobByID } from '../actions'
 
+import { Heading } from '@chakra-ui/react'
+
 export default function JobDetail() {
   const { id } = useParams()
 
+  const { job } = useSelector((state) => state.jobsReducer)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    dispatch(fetchJobByID(id))
+  }, [])
 
   return (
-    id
+    <Heading>{job.title}</Heading>
   )
 }
