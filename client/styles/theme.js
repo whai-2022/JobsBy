2a4c055d9ae91de1233a050dce165b0ca87f11b0
ben-marrett <benteiko@gmail.com>
2022-05-10 09:50:20 +1200

    remove temp background color

--- client/styles/theme.js
+++ client/styles/theme.js
@@ -14,7 +14,7 @@ const theme = extendTheme({
   styles: {
     global: {
       'html, body': {
-        backgroundColor: '#EDFDFD',
+        // backgroundColor: '#EDFDFD',
         lineHeight: 'tall',
       },
     },
