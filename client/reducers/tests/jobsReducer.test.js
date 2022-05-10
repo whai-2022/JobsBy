c79b1668bde58909067b1049b22314e4c53ea9ee
Anna An <annaan.golf@gmail.com>
2022-05-11 11:53:29 +1200

    Add more tests for jobsReducer

--- client/reducers/tests/jobsReducer.test.js
+++ client/reducers/tests/jobsReducer.test.js
@@ -15,7 +15,6 @@ describe('jobsReducer', () => {
         { id: 2, title: 'Job 2' }
       ] 
     }
-
     const expected = {
       jobs: [
         { id: 1, title: 'Job 1' },
@@ -32,13 +31,46 @@ describe('jobsReducer', () => {
       type: GET_JOB_DETAILS,
       payload: { id: 1, title: 'Job 1' }
     }
-
     const expected = {
       jobs: [],
       job: { id: 1, title: 'Job 1' },
       loading: false
     }
     const actual = jobsReducer(initialState, action)
+    expect(actual).toEqual(expected)
+  })
+  it('should correctly set the loading state to true', () => {
+    const action = {
+      type: JOB_LOADING
+    }
+    const expected = {
+      jobs: [],
+      job: {},
+      loading: true
+    }
+    const actual = jobsReducer(initialState, action)
+    expect(actual).toEqual(expected)
+  })
+  it('should correctly accept the job and update the jobs and job', () => {
+    const initialAcceptState = {
+      jobs: [
+        { id: 1, title: 'Old Job Title' }
+      ],
+      job: {},
+      loading: false
+    }
+    const action = {
+      type: ACCEPT_JOB,
+      payload: { id: 1, title: 'New Job Title' }
+    }
+    const expected = {
+      jobs: [
+        { id: 1, title: 'New Job Title' }
+      ],
+      job: { id: 1, title: 'New Job Title' },
+      loading: false
+    }
+    const actual = jobsReducer(initialAcceptState, action)
     expect(actual).toEqual(expected)
   })
 })
