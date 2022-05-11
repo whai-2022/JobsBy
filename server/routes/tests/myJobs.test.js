294054eee1f05c03fa55317d364f18dc8eb2d22b
Anna An <annaan.golf@gmail.com>
2022-05-11 15:47:26 +1200

    Add tests for myJobs routes

--- server/routes/tests/myJobs.test.js
+++ server/routes/tests/myJobs.test.js
@@ -4,33 +4,39 @@ const db = require('../../../server/db/db')
 
 jest.mock('../../../server/db/db')
 
+const jobsData = [
+  {
+    id: 1,
+    userId: 1,
+    accepted: false,
+    accepterId: 'Auth0||otherId',
+  },
+  {
+    id: 2,
+    userId: 1,
+    accepted: false,
+    accepterId: 'Auth0||otherId',
+  }
+]
+
 describe('GET /api/myJobs/jobsListed/:userId', () => {
-  it('should return all the correct jobs', () => {
-    const jobData = {
-      id: 1,
-      userId: 'Auth0||id',
-      name: 'Hilda',
-      email: 'google@gmail.com',
-      phone: '111',
-      title: 'Job listing',
-      description: 'It is job.',
-      requirements: '30 years experience',
-      type: 'voluntary',
-      region: 'Auckland',
-      lat: '-36.8505',
-      lon: '174.7645',
-      contactBy: 'This time',
-      occurrence: 'this occurred',
-      when: '01/01/1993',
-      pay: 'none',
-      accepted: false,
-      accepterId: 'Auth0||otherId',
-    }
-    db.getJobsByUserId.mockReturnValue(Promise.resolve(jobData))
+  it('should return the correct jobs', () => {
+    db.getJobsByUserId.mockReturnValue(Promise.resolve(jobsData))
     return request(server)
       .get('/api/myJobs/jobsListed/1')
       .then(res => {
-        expect(res.body).toEqual(jobData)
+        expect(res.body).toEqual(jobsData)
+      })
+  })
+})
+
+describe('GET /api/myJobs/jobsDone/:accepterId', () => {
+  it('should return the correct jobs', () => {
+    db.getJobsByAccepterId.mockReturnValue(Promise.resolve(jobsData))
+    return request(server)
+      .get('/api/myJobs/jobsDone/Auth0||otherId')
+      .then(res => {
+        expect(res.body).toEqual(jobsData)
       })
   })
 })
